ea9fb1dbf31d85902068dad72d3b4514c063bb63
Rachel Tairum <rachel.tairum@gmail.com>
2022-07-22 18:43:20 -0300

    style: Page lista ações

---
+++ desafio-xp-front/src/assets/styles/listaAcoesStyle.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const LayoutAcoes = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 35vh;
+`;
+
+export const TituloAcao = styled.h1`
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  width: 22vw;
+  height: 10vh;
+  text-align: center;
+  font-size: 3vw;
+  margin-top: 250px;
+`;
+
+export const Options = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 20px;
+`;
+
+export const ButtonOption = styled.button`
+  width: 10vw;
+  height: 5vh;
+  margin: 20px;
+  margin-top: 50px;
+  font-size: 1.2vw;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border-radius: 30px;
+  background-color: #576E75;
+  color: rgb(255, 255, 255);
+`;
+
+export const CarteiraButton = styled.button`
+  width: 12vw;
+  height: 30vh;
+  margin: 20px;
+  margin-top: 50px;
+  font-size: 1.2vw;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border-radius: 30px;
+  background-color: black;
+  color: rgb(255, 255, 255);
+  padding: 10px;
+`;
--- desafio-xp-front/src/pages/ListaAcoes.jsx
+++ desafio-xp-front/src/pages/ListaAcoes.jsx
@@ -4,6 +4,8 @@ import Acoes from '../Components/Acoes';
 import Header from '../Components/Header';
 import MinhasAcoes from '../Components/MinhasAcoes';
 
+import * as A from '../assets/styles/listaAcoesStyle';
+
 function ListaAcoes() {
   const [visibility, setVisibility] = useState('all');
   const history = useHistory();
@@ -11,13 +13,15 @@ function ListaAcoes() {
   return (
     <div>
       <Header />
-      <h1>Ações</h1>
-      <div>
-        <button type="button" onClick={() => setVisibility('mine')}>Minhas Ações</button>
-        <button type="button" onClick={() => setVisibility('all')}>Lista de ações</button>
-      </div>
-      {visibility === 'all' ? <Acoes /> : <MinhasAcoes />}
-      <button type="button" onClick={() => history.push('/carteira')}>Carteira Digital</button>
+      <A.LayoutAcoes>
+        <A.TituloAcao>Ações</A.TituloAcao>
+        <A.Options>
+          <A.ButtonOption type="button" onClick={() => setVisibility('mine')}>Minhas Ações</A.ButtonOption>
+          <A.ButtonOption type="button" onClick={() => setVisibility('all')}>Lista de ações</A.ButtonOption>
+        </A.Options>
+        {visibility === 'all' ? <Acoes /> : <MinhasAcoes />}
+        <A.CarteiraButton type="button" onClick={() => history.push('/carteira')}>Carteira Digital</A.CarteiraButton>
+      </A.LayoutAcoes>
     </div>
   );
 }
