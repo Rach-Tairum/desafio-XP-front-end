55c9eddb8c25c3c91858cbbc70967ccfce3cf888
Rachel Tairum <rachel.tairum@gmail.com>
2022-07-22 17:41:42 -0300

    style: header

--- desafio-xp-front/src/Components/Header.jsx
+++ desafio-xp-front/src/Components/Header.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { useContext, useEffect } from 'react';
 import { MyContext } from '../context/Provider';
+import { Cabecalho, NomeSaldo } from '../assets/styles/headerStyle';
 
 function Header() {
   const { idSaldo, name, getPersonInfos } = useContext(MyContext);
@@ -9,10 +10,19 @@ function Header() {
     getPersonInfos();
   }, [idSaldo]);
   return (
-    <div>
-      <h3>{ name }</h3>
-      <h3>{ idSaldo.saldo }</h3>
-    </div>
+    <Cabecalho>
+      <NomeSaldo>
+        Olá,
+        {' '}
+        { name }
+
+      </NomeSaldo>
+      <NomeSaldo>
+        Saldo:
+        {' '}
+        { idSaldo.saldo }
+      </NomeSaldo>
+    </Cabecalho>
   );
 }
 
---
+++ desafio-xp-front/src/assets/styles/headerStyle.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Cabecalho = styled.div`
+  background-color: #FFFFAD;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  height: 8vh;
+  border-bottom: 3px solid black;
+`;
+
+export const NomeSaldo = styled.h3`
+@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@200&family=Rubik&display=swap');
+  margin-right: 30px;
+  font-family: Rubik ;
+  font-size: 1.2vw;
+`;
