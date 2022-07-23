f09e3539d782600b29c2942388672240365139f6
Rachel Tairum <rachel.tairum@gmail.com>
2022-07-22 21:06:13 -0300

    style: page Carteira Digital

---
+++ desafio-xp-front/src/assets/styles/carteiraStyle.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const TituloCarteira = styled.h2`
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+width: 22vw;
+height: 10vh;
+text-align: center;
+font-size: 3vw;
+margin-top: 260px;
+`;
+
+export const RotuloValor = styled.label`
+  @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@200&display=swap');
+  font-family: Prompt ;
+  font-size: 1.5vw;
+  padding: 10px;
+`;
+
+export const InputConta = styled.input`
+@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@200&display=swap');
+  font-family: Prompt ;
+  font-weight: 600;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-size: 0.9vw;
+  margin-right: 5px;
+`;
+
+export const ContainerCarteira = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 15vh;
+`;
--- desafio-xp-front/src/pages/CarteiraDigital.jsx
+++ desafio-xp-front/src/pages/CarteiraDigital.jsx
@@ -3,17 +3,22 @@ import Deposito from '../Components/Deposito';
 import Header from '../Components/Header';
 import Saque from '../Components/Saque';
 
+import { TituloCarteira, ContainerCarteira } from '../assets/styles/carteiraStyle';
+import * as A from '../assets/styles/listaAcoesStyle';
+
 function CarteiraDigital() {
   const [visibility, setVisibility] = useState('saque');
   return (
     <div>
       <Header />
-      <h2>Carteira Digital</h2>
-      <div>
-        <button type="button" onClick={() => setVisibility('deposito')}>Deposito</button>
-        <button type="button" onClick={() => setVisibility('saque')}>Saque</button>
-      </div>
-      {visibility === 'saque' ? <Saque /> : <Deposito />}
+      <ContainerCarteira>
+        <TituloCarteira>Carteira Digital</TituloCarteira>
+        <A.Options>
+          <A.ButtonOption type="button" onClick={() => setVisibility('deposito')}>Deposito</A.ButtonOption>
+          <A.ButtonOption type="button" onClick={() => setVisibility('saque')}>Saque</A.ButtonOption>
+        </A.Options>
+        {visibility === 'saque' ? <Saque /> : <Deposito />}
+      </ContainerCarteira>
     </div>
   );
 }
